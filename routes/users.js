e15a5e05e89351767446c6faa97204aa654c8156
Warnsley <Marcus.Warnsley@navient.com>
2016-04-09 14:04:54 -0400

    Adding the post method for the registration page

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var multer = require("multer");
+var upload = multer({dest: './uploads'});
 var router = express.Router();
 
 /* GET users listing. */
@@ -13,4 +15,22 @@ router.get('/register', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login', {title:'Login'});
 });
+
+router.post('/register', upload.single('profileimage'), function(req, res, next){
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	if(req.file){
+		console.log("Uploading File...");
+	}else{
+		console.log("No File Uploaded...");
+	}
+
+	console.log(req.file);
+
+});;
+
 module.exports = router;
