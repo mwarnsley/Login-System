d808ca78afecd0d3f60ddd642cdc0d8832345359
Warnsley <Marcus.Warnsley@navient.com>
2016-04-09 13:29:42 -0400

    Adding more middleware and the express validator

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require("express-session");
 var passport = require("passport");
+var expressValidator = require('express-validator');
 var localStrategy = require("passport-local").Strategy;
 var multer = require("multer");
+var upload = multer({dest: './uploads'});
 var flash = require("connect-flash");
 var mongo = require("mongodb");
 var mongoose = require("mongoose");
@@ -21,9 +23,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-// Handle the files uploads
-app.use(multer({dest: './uploads'}));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -61,6 +60,12 @@ app.use(expressValidator({
     };
   }
 }));
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
